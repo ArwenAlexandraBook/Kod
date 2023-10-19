b93cd2c7d0ab84296a65a2326e013d806a49d34e
Arwen Alexandra <90157855+ArwenAlexandraBook@users.noreply.github.com>
2023-10-19 12:30:59 +0200

    Add files via upload

---
+++ src/lcd.h
@@ -0,0 +1,89 @@
+#ifndef LCD_H
+#define LCD_H
+
+#include <stdint.h>
+#include <avr/io.h>
+
+#define LCD_PORT PORTD
+#define LCD_DDR DDRD 
+
+#if (!defined LCD_DDR || !defined LCD_PORT)
+#warning "Var god definera LCD_DDR och LCD_PORT"
+#define LCD_DDR DDRB
+#define LCD_PORT PORTB
+#endif 
+
+#ifndef LCD_RS
+#define LCD_RS_PIN 2  // GPIO-pin till RS
+#define LCD_EN_PIN 3  // GPIO-pin till EN 
+#define LCD_D4_PIN 4  // GPIO-pin till D4
+#define LCD_D5_PIN 5  // GPIO-pin till D5
+#define LCD_D6_PIN 6  // GPIO-pin till D6
+#define LCD_D7_PIN 7  // GPIO-pin till D7
+
+ 
+#define COL_COUNT 16  //Kolumner på LCD skärmen
+#define LCD_ROW_COUNT 2  //Amtalet rader på LCD_skärmen
+
+
+#define LCD_CLEARDISPLAY 0x01
+#define LCD_RETURNHOME 0x02
+#define LCD_ENTRYMODESET 0x04
+#define LCD_DISPLAYCONTROL 0x08
+#define LCD_CURSORSHIFT 0x10
+#define LCD_FUNCTIONSET 0X20
+#define LCD_SETDDRAMADDR 0x80 
+
+#define LCD_ENTRYRIGHT 0x00 
+#define LCD_ENTRYLEFT 0x02
+#define LCD_ENTRYSHIFTINCREMENT 0x01
+#define LCD_ENTRYSHIFTDECREMENT 0x00
+
+#define LCD_DISPLAYON 0x04
+#define LCD_DISPLAYOFF 0x00 
+#define LCD_CURSORON 0x01
+#define LCD_BLINKOFF 0x00
+
+#define LCD_DISPLAYMOVE 0x08
+#define LCD_CURSORMODE 0x00
+#define LCD_MOVERIGHT 0x04
+#define LCD_MOVELEFT 0x00 
+
+#define LCD_8BITMODE 0x10
+#define LCD_4BITMODE 0x00
+#define LCD_2LINE 0x08
+#define LCD_1LINE 0x00
+#define LCD_5x10DOTS 0x04
+#define LCD_5x8DOTS 0x00
+
+
+// Nedan följer funktioner för hantering av LCD.
+void lcd_init(void);
+
+void lcd_clear(void);
+
+void lcd_command(uint_ command); 
+
+void lcd_on(void);
+void lcd_off(void);
+
+void lcd_return_home(void);
+
+void lcd_enable_blinking(void);
+void lcd_disable_blinking(void);
+
+void lcd_enable_cursor(void);
+void lcd_disable_cursor(void);
+
+void lcd_scroll_left(void);
+void lcd_scroll_right(void);
+
+void lcd_enable_autoscroll(void);
+void lcd_disable_autoscroll(void);
+
+void lcd_create_char(uint8_t location, uint8_t *charmap); 
+
+void lcd_set_cursor(uint8_t col, uint8_t row);
+
+void lcd_puts(char *string);
+void lcd_printf(char *format, ...);
---
+++ src/main.c
@@ -0,0 +1,137 @@
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+#include <stdio.h>
+#include <string.h>
+#include "lcd.h"
+#include "esp8266.h"
+
+#define RED_BUTTON_PIN 2    // Röda knappens pin
+#define YELLOW_BUTTON_PIN 4 // Gula knappens pin
+#define GREEN_BUTTON_PIN 5  // Gröna knappens pin
+
+const int buttonPin[] = {RED_BUTTON_PIN, YELLOW_BUTTON_PIN, GREEN_BUTTON_PIN};
+const int numButton = sizeof(buttonPin) / sizeof(buttonPin[0]);
+
+char currentText[5] = {0};
+char questionText[20] = " "; 
+
+// Specificerar SSID och lösenord för lokala wifinätverk
+
+const char *wifiSSIDs[] = {"SSID1"} //Det SSID man använder. 
+const char *wifiPassWords[] = {"Password1"}; //Lösenord till wifi
+const int numNetworks = sizeof(wifiSSIDs) /sizof(wifiSSIDs[0]);
+
+struct ButtonEvent
+{
+
+ int button;
+ unsigned long timestamp;
+};
+
+struct ButtonEvent buttonEvents[60];
+int numEvents = 0;
+
+bool ConnectToWifi(const char *ssid, const char *password)
+{
+wifi_set_opmode(STATION_MODE);
+
+struct station_config stationConf;
+memset(&statonConf, 0, sizeof(struct station_config));
+strcpy(stationConf.ssid,wifiSSID[0]);  // Ange det wifi man ska logga in på.
+strcpy(stationConf.password,wifiPassWord[0]); //Ange lösenord
+
+wifi_station_set_config(&stationConf);
+wifi_station_connect();
+
+
+int timeout = 10000; // 10 sekunders väntan på om anslutningen till wifi lyckas
+while (wifi_station_get_connect_status()! = STATION_GOT_IP && timeout > 0)
+{
+    _delay_us(100000); 
+    timeout -=1000;
+}
+
+if (wifi_station_get_connect_status() === STATION_GOT_IP)
+{
+ return true;
+}
+else 
+{
+    return false;
+}
+}
+void HandleButtonClick(char *txt)
+{
+    _delay_us(200000); //Förfröjning mikrosekunder
+    strncat(currentText, txt, sizeof(currentText) - strlen(currentText) - 1);
+    lcd_set_cursor(0, 1);
+    lcd_puts(currentText);
+
+    if (strlen(currentText) == 4)
+    {
+        if (strcmp("1442", currentText) == 0)
+        {
+            lcd_printf("That's correct!");
+        }
+        else
+        {
+            lcd_printf("Incorrct code!"); // Felmeddelande
+            _delay_us(300000); //Fördröjning i mikrosekunder
+            lcd_set_cursor(0, 1); // LCD-cursor rad 1
+            lcd_puts("  "); //Rensar raden
+            lcd_set_cursor(0, 1); //Återgår till rad 1
+        }
+        currentText[0] = 0;
+    }
+}
+
+void ConnectToWiFiNetworks()
+{
+
+for (int i = 0; i < numNetworks;i++)  //Söker efter tillgänliga nätverk.
+{
+    if (ConnectToWifi(wifiSSIDs[i], wifiPassWords[i]))
+    {
+
+        break;
+    }
+}
+
+}
+
+void setup()
+{
+    for (int i = 0; i < numButton; i++)
+    {
+        pinMode(buttonPin[i], INPUT_PULLUP);
+    }
+
+    lcd_init();            // Initialiserar LCD
+    lcd_enable_blinking(); // Aktiverar blink
+    lcd_enable_cursor();   // Aktiverar cursor
+
+    lcd_set_cursor(0, 0);   // LCD-cursor rad 0
+    lcd_puts(questionText); // Frågan visas på lcd skärmen
+
+    ConnectToWifiNetworks();
+}
+
+void loop()
+{
+
+    for (int i = 0; i < numButton; i++)
+    {
+        if (digitalRead(buttonPin[i]) == LOW)
+        {
+
+            if (numEvents < sizeof(buttonEvents) / sizeof(buttonEvents[0]))
+            {
+                buttonEvents[numEvents].button = i;
+            buttonEvents[numEvents].timestamp = millis();
+            numEvents++;
+        }
+    }
+}
+}
