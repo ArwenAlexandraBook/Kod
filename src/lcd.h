34c33931eacfb87c278d4d4e712cf0f4f4d3fe4e
Arwen Alexandra <90157855+ArwenAlexandraBook@users.noreply.github.com>
2023-10-24 19:15:33 +0200

    Update lcd.h

--- src/lcd.h
+++ src/lcd.h
@@ -1,30 +1,28 @@
-#ifndef LCD_H
-#define LCD_H
-
-#include <stdint.h>
 #include <avr/io.h>
 
 #define LCD_PORT PORTD
-#define LCD_DDR DDRD 
+#define LCD_DDR DDRD
 
 #if (!defined LCD_DDR || !defined LCD_PORT)
-#warning "Var god definera LCD_DDR och LCD_PORT"
+#warning "Please define LCD_DDR and LCD_PORT"
 #define LCD_DDR DDRB
 #define LCD_PORT PORTB
-#endif 
+#endif
 
 #ifndef LCD_RS
-#define LCD_RS_PIN 2  // GPIO-pin till RS
-#define LCD_EN_PIN 3  // GPIO-pin till EN 
-#define LCD_D4_PIN 4  // GPIO-pin till D4
-#define LCD_D5_PIN 5  // GPIO-pin till D5
-#define LCD_D6_PIN 6  // GPIO-pin till D6
-#define LCD_D7_PIN 7  // GPIO-pin till D7
+#warning "LCD using default pin".
+#define LCD_RS 0
+#define LCD_RW 2
+#define LCD_EN 2
+#define LCD_D0 4
+#define LCD_D1 5
+#define LCD_D2 6
+#define LCD_D3 7
 
- 
-#define COL_COUNT 16  //Kolumner på LCD skärmen
-#define LCD_ROW_COUNT 2  //Amtalet rader på LCD_skärmen
+#endif
 
+#define LCD_COL_COUNT 16 // Kolumner på LCD skärmen
+#define LCD_ROW_COUNT 2  // Amtalet rader på LCD_skärmen
 
 #define LCD_CLEARDISPLAY 0x01
 #define LCD_RETURNHOME 0x02
@@ -32,22 +30,22 @@
 #define LCD_DISPLAYCONTROL 0x08
 #define LCD_CURSORSHIFT 0x10
 #define LCD_FUNCTIONSET 0X20
-#define LCD_SETDDRAMADDR 0x80 
+#define LCD_SETDDRAMADDR 0x80
 
-#define LCD_ENTRYRIGHT 0x00 
+#define LCD_ENTRYRIGHT 0x00
 #define LCD_ENTRYLEFT 0x02
 #define LCD_ENTRYSHIFTINCREMENT 0x01
 #define LCD_ENTRYSHIFTDECREMENT 0x00
 
 #define LCD_DISPLAYON 0x04
-#define LCD_DISPLAYOFF 0x00 
+#define LCD_DISPLAYOFF 0x00
 #define LCD_CURSORON 0x01
 #define LCD_BLINKOFF 0x00
 
 #define LCD_DISPLAYMOVE 0x08
 #define LCD_CURSORMODE 0x00
 #define LCD_MOVERIGHT 0x04
-#define LCD_MOVELEFT 0x00 
+#define LCD_MOVELEFT 0x00
 
 #define LCD_8BITMODE 0x10
 #define LCD_4BITMODE 0x00
@@ -56,13 +54,13 @@
 #define LCD_5x10DOTS 0x04
 #define LCD_5x8DOTS 0x00
 
-
 // Nedan följer funktioner för hantering av LCD.
 void lcd_init(void);
 
 void lcd_clear(void);
 
-void lcd_command(uint_ command); 
+void lcd_command(uint8_t command);
+void lcd_write(uint8_t value);
 
 void lcd_on(void);
 void lcd_off(void);
@@ -81,9 +79,10 @@ void lcd_scroll_right(void);
 void lcd_enable_autoscroll(void);
 void lcd_disable_autoscroll(void);
 
-void lcd_create_char(uint8_t location, uint8_t *charmap); 
+void lcd_create_char(uint8_t location, uint8_t *charmap);
 
 void lcd_set_cursor(uint8_t col, uint8_t row);
 
 void lcd_puts(char *string);
 void lcd_printf(char *format, ...);
+
