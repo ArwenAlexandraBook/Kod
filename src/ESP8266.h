aa1354d358deb962480a12c2b0a8b535821cd0b5
Arwen Alexandra <90157855+ArwenAlexandraBook@users.noreply.github.com>
2023-10-24 18:14:02 +0200

    Update ESP8266.h

--- src/ESP8266.h
+++ src/ESP8266.h
@@ -1,7 +1,7 @@
 #ifndef ESP8266_H
 #define ESP8266_H
 
-void esp8266_init(void);  //Initierar ESP8266
+void ESPinit(void);  //Initierar ESP8266
 void waitFor (char* response); // Vänta på svar från ESP8266
 void addData(char* name ,int data); // Lägga till data
 void pushData(void); //Skicka data från ESP8266
